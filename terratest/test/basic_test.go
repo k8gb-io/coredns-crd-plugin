332f163a410b970886eb20d4c8e5f82852ffceb5
Dinar Valeev <dinar.valeev@absa.africa>
2021-03-28 23:53:00 +0200

    Add tests for AAAA and TTL
    * run code through gofmt and goimports
    * switch to github.com/miekg/dns for DNS queries
    * add AAAA and TTL tests
    
    Signed-off-by: Dinar Valeev <dinar.valeev@absa.africa>

--- terratest/test/basic_test.go
+++ terratest/test/basic_test.go
@@ -19,50 +19,51 @@ package test
 
 import (
 	"fmt"
-        "path/filepath"
-        "strings"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-        "github.com/gruntwork-io/terratest/modules/k8s"
-        "github.com/gruntwork-io/terratest/modules/random"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const mallformedPacket = ";; Warning: Message parser reports malformed message packet."
-
 func TestBasicExample(t *testing.T) {
-        t.Parallel()
+	t.Parallel()
 
 	var coreDNSPods []corev1.Pod
 
-        // Path to the Kubernetes resource config we will test
-        kubeResourcePath, err := filepath.Abs("../example/dnsendpoint.yaml")
-        require.NoError(t, err)
+	// Path to the Kubernetes resource config we will test
+	kubeResourcePath, err := filepath.Abs("../example/dnsendpoint.yaml")
+	require.NoError(t, err)
+	ttlEndpoint, err := filepath.Abs("../example/dnsendpoint_ttl.yaml")
+	require.NoError(t, err)
 	brokenEndpoint, err := filepath.Abs("../example/dnsendpoint_broken.yaml")
-        require.NoError(t, err)
+	require.NoError(t, err)
 
-        // To ensure we can reuse the resource config on the same cluster to test different scenarios, we setup a unique
-        // namespace for the resources for this test.
-        // Note that namespaces must be lowercase.
-        namespaceName := fmt.Sprintf("coredns-test-%s", strings.ToLower(random.UniqueId()))
+	// To ensure we can reuse the resource config on the same cluster to test different scenarios, we setup a unique
+	// namespace for the resources for this test.
+	// Note that namespaces must be lowercase.
+	namespaceName := fmt.Sprintf("coredns-test-%s", strings.ToLower(random.UniqueId()))
 
-        options := k8s.NewKubectlOptions("", "", namespaceName)
+	options := k8s.NewKubectlOptions("", "", namespaceName)
 	mainNsOptions := k8s.NewKubectlOptions("", "", "coredns")
 	podFilter := metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=coredns",
 	}
 
-        k8s.CreateNamespace(t, options, namespaceName)
+	k8s.CreateNamespace(t, options, namespaceName)
 
-        defer k8s.DeleteNamespace(t, options, namespaceName)
+	defer k8s.DeleteNamespace(t, options, namespaceName)
 
-        defer k8s.KubectlDelete(t, options, kubeResourcePath)
+	defer k8s.KubectlDelete(t, options, kubeResourcePath)
 
-        k8s.KubectlApply(t, options, kubeResourcePath)
+	k8s.KubectlApply(t, options, kubeResourcePath)
 
 	k8s.WaitUntilNumPodsCreated(t, mainNsOptions, podFilter, 1, 60, 1*time.Second)
 
@@ -71,21 +72,44 @@ func TestBasicExample(t *testing.T) {
 	for _, pod := range coreDNSPods {
 		k8s.WaitUntilPodAvailable(t, mainNsOptions, pod.Name, 60, 1*time.Second)
 	}
-	actualIP, _ := Dig(t, "localhost", 1053, "host1.example.org")
 
-	assert.Contains(t, actualIP, "1.2.3.4")
+	t.Run("Basic type A resolve", func(t *testing.T) {
+		actualIP, err := DigIPs(t, "localhost", 1053, "host1.example.org", dns.TypeA)
+		require.NoError(t, err)
+		assert.Contains(t, actualIP, "1.2.3.4")
+	})
 
 	// check for NODATA replay on non labeled endpoints
-	emptyIP, _ := Dig(t, "localhost", 1053, "host3.example.org")
-	assert.NotContains(t, emptyIP,"1.2.3.4" )
+	t.Run("NODATA reply on non labeled endpoints", func(t *testing.T) {
+		emptyIP, err := DigIPs(t, "localhost", 1053, "host3.example.org", dns.TypeA)
+		require.NoError(t, err)
+		assert.NotContains(t, emptyIP, "1.2.3.4")
+	})
 
-	// Validate artificial(broken) DNS doesn't break CoreDNS
-	k8s.KubectlApply(t, options, brokenEndpoint)
-	brokenData1, _ := Dig(t, "localhost", 1053, "broken1.example.org")
-	assert.Contains(t, brokenData1, mallformedPacket)
-	brokenData2, _ := Dig(t, "localhost", 1053, "broken2.example.org")
-	assert.Contains(t, brokenData2, mallformedPacket)
-	// We still able to get "healthy" records
-	currentIP, _ := Dig(t, "localhost", 1053, "host1.example.org")
-	assert.Contains(t, currentIP, "1.2.3.4")
+	t.Run("Validate artificial(broken) DNS doesn't break CoreDNS", func(t *testing.T) {
+		k8s.KubectlApply(t, options, brokenEndpoint)
+		_, err := DigIPs(t, "localhost", 1053, "broken1.example.org", dns.TypeA)
+		require.Error(t, err)
+		_, err = DigIPs(t, "localhost", 1053, "broken2.example.org", dns.TypeA)
+		require.Error(t, err)
+
+		// We still able to get "healthy" records
+		currentIP, err := DigIPs(t, "localhost", 1053, "host1.example.org", dns.TypeA)
+		require.NoError(t, err)
+		assert.Contains(t, currentIP, "1.2.3.4")
+	})
+
+	t.Run("TTL is correctly evaluated", func(t *testing.T) {
+		k8s.KubectlApply(t, options, ttlEndpoint)
+		msg, err := DigMsg(t, "localhost", 1053, "ttl.example.org", dns.TypeA)
+
+		require.NoError(t, err)
+		assert.Equal(t, uint32(123), msg.Answer[0].(*dns.A).Hdr.Ttl)
+	})
+
+	t.Run("Type AAAA returns Rcode 3", func(t *testing.T) {
+		msg, err := DigMsg(t, "localhost", 1053, "host1.example.org", dns.TypeAAAA)
+		require.NoError(t, err)
+		assert.Equal(t, dns.RcodeNameError, msg.Rcode)
+	})
 }
