283066b5525831ff7056a18ab6723a7480c7bfc1
Dinar Valeev <dinar.valeev@absa.africa>
2021-03-29 21:03:45 +0200

    run through imports and formaters

--- terratest/test/basic_test.go
+++ terratest/test/basic_test.go
@@ -19,50 +19,50 @@ package test
 
 import (
 	"fmt"
-        "path/filepath"
-        "strings"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-        "github.com/gruntwork-io/terratest/modules/k8s"
-        "github.com/gruntwork-io/terratest/modules/random"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const mallformedPacket = ";; Warning: Message parser reports malformed message packet."
 
 func TestBasicExample(t *testing.T) {
-        t.Parallel()
+	t.Parallel()
 
 	var coreDNSPods []corev1.Pod
 
-        // Path to the Kubernetes resource config we will test
-        kubeResourcePath, err := filepath.Abs("../example/dnsendpoint.yaml")
-        require.NoError(t, err)
+	// Path to the Kubernetes resource config we will test
+	kubeResourcePath, err := filepath.Abs("../example/dnsendpoint.yaml")
+	require.NoError(t, err)
 	brokenEndpoint, err := filepath.Abs("../example/dnsendpoint_broken.yaml")
-        require.NoError(t, err)
+	require.NoError(t, err)
 
-        // To ensure we can reuse the resource config on the same cluster to test different scenarios, we setup a unique
-        // namespace for the resources for this test.
-        // Note that namespaces must be lowercase.
-        namespaceName := fmt.Sprintf("coredns-test-%s", strings.ToLower(random.UniqueId()))
+	// To ensure we can reuse the resource config on the same cluster to test different scenarios, we setup a unique
+	// namespace for the resources for this test.
+	// Note that namespaces must be lowercase.
+	namespaceName := fmt.Sprintf("coredns-test-%s", strings.ToLower(random.UniqueId()))
 
-        options := k8s.NewKubectlOptions("", "", namespaceName)
+	options := k8s.NewKubectlOptions("", "", namespaceName)
 	mainNsOptions := k8s.NewKubectlOptions("", "", "coredns")
 	podFilter := metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=coredns",
 	}
 
-        k8s.CreateNamespace(t, options, namespaceName)
+	k8s.CreateNamespace(t, options, namespaceName)
 
-        defer k8s.DeleteNamespace(t, options, namespaceName)
+	defer k8s.DeleteNamespace(t, options, namespaceName)
 
-        defer k8s.KubectlDelete(t, options, kubeResourcePath)
+	defer k8s.KubectlDelete(t, options, kubeResourcePath)
 
-        k8s.KubectlApply(t, options, kubeResourcePath)
+	k8s.KubectlApply(t, options, kubeResourcePath)
 
 	k8s.WaitUntilNumPodsCreated(t, mainNsOptions, podFilter, 1, 60, 1*time.Second)
 
@@ -77,7 +77,7 @@ func TestBasicExample(t *testing.T) {
 
 	// check for NODATA replay on non labeled endpoints
 	emptyIP, _ := Dig(t, "localhost", 1053, "host3.example.org")
-	assert.NotContains(t, emptyIP,"1.2.3.4" )
+	assert.NotContains(t, emptyIP, "1.2.3.4")
 
 	// Validate artificial(broken) DNS doesn't break CoreDNS
 	k8s.KubectlApply(t, options, brokenEndpoint)
