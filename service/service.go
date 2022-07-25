a374c31dc16d719fd6e46fcdb71ba60f928415a2
kuritka <kuritka@gmail.com>
2022-07-25 15:44:47 +0200

    linting issues 2

--- service/service.go
+++ service/service.go
@@ -13,6 +13,6 @@ type PluginContainer interface {
 	Add(plugin.Handler) error
 	// Execute run plugin.Handler.ServeDNS in order as it was added.
 	// If error occurs, it exits immediately end return error.
-	// If !dns.RcodeSuccess is returned, it exits immediatelly end return error.
+	// If !dns.RcodeSuccess is returned, it exits immediately end return error.
 	Execute(ctx context.Context, w dns.ResponseWriter, msg *dns.Msg) error
 }
