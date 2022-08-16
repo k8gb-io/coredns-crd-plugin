72b9bf84f7988d1e08919575cfaf6933fdbe4d7d
kuritka <kuritka@gmail.com>
2022-08-16 17:52:18 +0200

    extended container tests

--- service/container_test.go
+++ service/container_test.go
@@ -2,51 +2,114 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/coredns/coredns/plugin"
 	"github.com/golang/mock/gomock"
-	"github.com/miekg/dns"
+	dns "github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExecute(t *testing.T) {
+type fakePlugin struct {
+	handler plugin.Handler
+}
 
+type fakeWriter struct {
+	writer dns.ResponseWriter
+}
+
+func newFakeHandler(ctrl *gomock.Controller, f func(h *MockHandler)) fakePlugin {
+	h := NewMockHandler(ctrl)
+	f(h)
+	return fakePlugin{
+		handler: h,
+	}
+}
+
+func newFakeWriter(ctrl *gomock.Controller, f func(w *MockResponseWriter)) fakeWriter {
+	w := NewMockResponseWriter(ctrl)
+	f(w)
+	return fakeWriter{
+		writer: w,
+	}
+}
+
+func TestContainer(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	m := NewMockHandler(ctrl)
-	w := NewMockResponseWriter(ctrl)
+
 	tests := []struct {
-		name     string
-		handlers []struct {
-			handler plugin.Handler
-			f       *gomock.Call
-			w       *gomock.Call
-		}
+		name          string
+		expectedError bool
+		writer        fakeWriter
+		plugin        []fakePlugin
 	}{
 		{
-			name: "oneHandlerSucceed",
-			handlers: []struct {
-				handler plugin.Handler
-				f       *gomock.Call
-				w       *gomock.Call
-			}{
-				{
-					handler: m,
-					f:       m.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, nil),
-					w:       w.EXPECT().WriteMsg(gomock.Any()).Return(nil),
-				},
-			},
+			name: "OneHandlerSucceed",
+			writer: newFakeWriter(ctrl, func(w *MockResponseWriter) {
+				w.EXPECT().WriteMsg(gomock.Any()).Return(nil).Times(1)
+			}),
+			plugin: []fakePlugin{
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, nil).Times(1)
+				})},
+		},
+		{
+			name: "TwoHandlersSucceed",
+			writer: newFakeWriter(ctrl, func(w *MockResponseWriter) {
+				w.EXPECT().WriteMsg(gomock.Any()).Return(nil).Times(1)
+			}),
+			plugin: []fakePlugin{
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, nil).Times(1)
+				}),
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, nil).Times(1)
+				})},
+		},
+		{
+			name: "NoHandlers",
+			writer: newFakeWriter(ctrl, func(w *MockResponseWriter) {
+				w.EXPECT().WriteMsg(gomock.Any()).Return(nil).Times(1)
+			}),
+			plugin: []fakePlugin{},
+		},
+		{
+			name:          "FirstHandlerError",
+			expectedError: true,
+			writer:        newFakeWriter(ctrl, func(w *MockResponseWriter) {}),
+			plugin: []fakePlugin{
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().Name().Return("fake").Times(1)
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, fmt.Errorf("fake")).Times(1)
+				}),
+				newFakeHandler(ctrl, func(h *MockHandler) {})},
+		},
+		{
+			name:          "SecondHandlerError",
+			expectedError: true,
+			writer:        newFakeWriter(ctrl, func(w *MockResponseWriter) {}),
+			plugin: []fakePlugin{
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, nil).Times(1)
+				}),
+				newFakeHandler(ctrl, func(h *MockHandler) {
+					h.EXPECT().Name().Return("fake").Times(1)
+					h.EXPECT().ServeDNS(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.RcodeSuccess, fmt.Errorf("fake")).Times(1)
+				})},
 		},
 	}
 
 	for _, test := range tests {
-		c := NewCommonContainer()
-		for _, h := range test.handlers {
-			err := c.Register(h.handler)
-			assert.NoError(t, err)
-		}
-		err := c.Execute(context.TODO(), w, &dns.Msg{})
-		assert.NoError(t, err)
+		t.Run(test.name, func(t *testing.T) {
+			c := NewCommonContainer()
+			for _, p := range test.plugin {
+				err := c.Register(p.handler)
+				assert.NoError(t, err)
+			}
+			err := c.Execute(context.TODO(), test.writer.writer, &dns.Msg{})
+			assert.Equal(t, test.expectedError, err != nil)
+		})
 	}
 }
